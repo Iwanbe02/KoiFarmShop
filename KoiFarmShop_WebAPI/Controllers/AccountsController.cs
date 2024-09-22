497ac5f68b115ec4bed6ca62cfc9da2a0b201eda
TrongTLH <truonghieutrongle@gmail.com>
2024-09-22 09:41:09 +0700

    CRUD_Account

---
+++ KoiFarmShop_WebAPI/Controllers/AccountsController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BusinessObjects.Models;
+using Services.Interface;
+using DataAccessObjects.DTOs.AccountDTO;
+using Services.Implement;
+
+namespace KoiFarmShop_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountsController(IAccountService _accountService)
+        {
+            _accountService = _accountService;
+        }
+
+        // GET: api/Accounts
+        [HttpGet]
+        public async Task<IActionResult> GetAllAccounts()
+        {
+            var account = await _accountService.GetAllAccounts();
+            return Ok(account);
+        }
+
+        // GET: api/Accounts/5
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetAccountById(int Id)
+        {
+            var account = await _accountService.GetAccountById(Id);
+            return Ok(account);
+        }
+
+
+        // POST: api/Accounts
+        
+        [HttpPost]
+        public async Task<ActionResult<Account>> CreateAccount(CreateAccountDTO createAccountDTO) 
+        {
+            var account = await _accountService.CreateAccount(createAccountDTO);
+            return Ok(account);
+        }
+
+        // PUT: api/Accounts/5
+        [HttpPut("{Id")]
+
+        public async Task<IActionResult> UpdateAccount(int Id, UpdateAccountDTO updateAccountDTO)
+        {
+            var account = await _accountService.UpdateAccount(Id, updateAccountDTO);
+            return Ok(account);
+        }
+
+        [HttpPut("{Id}/{isDeleted}")]
+        public async Task<IActionResult> RestoreAccount(int Id)
+        {
+            await _accountService.RestoreAccount(Id);
+            return Ok();
+        }
+
+        // DELETE: api/Accounts/5
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> DeleteAccount(int Id)
+        {
+            var account = await _accountService.DeleteAccount(Id);
+            return Ok(account);
+        }
+
+    }
+}
---
+++ Services/Interface/IAccountService.cs
@@ -0,0 +1,20 @@
+﻿using BusinessObjects.Models;
+using DataAccessObjects.DTOs.AccountDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interface
+{
+    public interface IAccountService
+    {
+        Task<IEnumerable<Account>> GetAllAccounts();
+        Task<Account> GetAccountById(int id);
+        Task<Account> CreateAccount(CreateAccountDTO createAccount);
+        Task<Account> UpdateAccount(int id, UpdateAccountDTO updateAccount);
+        Task<Account> DeleteAccount(int id);
+        Task<Account> RestoreAccount(int id);
+    }
+}
