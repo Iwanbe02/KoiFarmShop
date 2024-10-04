8a015f0df590453f618f70f3ed021f376ffdcb15
TrongTLH <truonghieutrongle@gmail.com>
2024-10-04 18:01:08 +0700

    Origincertificate

---
+++ DataAccessObjects/DTOs/OrriginCetificateDTO/CreateOriginCertificateDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessObjects.DTOs.OrriginCetificateDTO
+{
+    public class CreateOriginCertificateDTO
+    {
+        public int? KoiId { get; set; }
+
+        public int? OrderId { get; set; }
+
+        public DateTime? StartTime { get; set; }
+
+        public DateTime? EndTime { get; set; }
+
+    }
+}
---
+++ Services/Interface/IOriginCertificateService.cs
@@ -0,0 +1,21 @@
+﻿using BusinessObjects.Models;
+using DataAccessObjects.DTOs.KoiFishDTO;
+using DataAccessObjects.DTOs.OrriginCetificateDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interface
+{
+    public interface IOriginCertificateService
+    {
+        Task<IEnumerable<OriginCertificate>> GetAllOriginCertificate();
+        Task<OriginCertificate> GetOriginCertificateById(int id);
+        Task<OriginCertificate> CreateOriginCertificate(CreateOriginCertificateDTO createOriginCertificate);
+        Task<OriginCertificate> UpdateOriginCertificate(int id, UpdateOriginCertificateDTO updateOriginCertificate);
+        Task<OriginCertificate> DeleteOriginCertificate(int id);
+        Task<OriginCertificate> RestoreOriginCertificate(int id);
+    }
+}
